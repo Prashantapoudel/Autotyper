f5fc4b4034590bf23bbc69546d467647b594f761
prashanta poudel <93674307+Prashantapoudel@users.noreply.github.com>
2023-11-06 19:49:09 -0500

    Update autotyper.py

--- autotyper.py
+++ autotyper.py
@@ -1,7 +1,15 @@
-import time
-import pyautogui as pg
+#Writing a python code that autotypes something that has been on program and press enter automatically.
+# it types at the curser blinking.
 
-for i in range(200):
-    time.sleep(5)
-    pg.write("hello world")
-    pg.press('Enter')
+
+#importing the necessary library and modules.
+
+
+import time #imorting time to know the sleep time
+#importing pyautogui for the mouse point and pressing enter.
+import pyautogui as pg #shortening the pyautogui as pg
+
+for i in range(200): #Starting the loop for how many times you want to print that.
+    time.sleep(5)# waiting time after typing
+    pg.write("hello world")# text you want to type
+    pg.press('Enter') # does the work of enter button on keyboard and ready for another text after 5 sec
